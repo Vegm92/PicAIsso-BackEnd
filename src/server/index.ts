7ed403db8f5a981a9b9d22fda68bce61bff5a19e
Victor Granda <victorgranda1992@gmail.com>
2023-03-06 19:03:04 +0100

    Add index at server folder, express, morgan and cors

---
+++ src/server/index.ts
@@ -0,0 +1,11 @@
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
+
+export const app = express();
+
+app.disable("x-powered-by");
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
