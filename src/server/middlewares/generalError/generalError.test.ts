40530946637b3806eba5da5dd43566e073481aad
Victor Granda <victorgranda1992@gmail.com>
2023-03-08 19:31:58 +0100

    Add generalError suit

--- src/server/middlewares/generalError/generalError.test.ts
+++ src/server/middlewares/generalError/generalError.test.ts
@@ -1,4 +1,5 @@
 import { type NextFunction, type Request, type Response } from "express";
+import { ValidationError, type errors } from "express-validation";
 import CustomError from "../../../CustomError/CustomError";
 import { generalError } from "./generalError";
 const res: Partial<Response> = {
@@ -23,6 +24,43 @@ describe("Given a generalError middleware", () => {
       generalError(error, req as Request, res as Response, next);
 
       expect(res.status).toHaveBeenCalledWith(statusCode);
+    });
+  });
+
+  describe("When it receives an error object generated due to missing password in credentials", () => {
+    test("Then the erro public message will be '\"password\" is required'", () => {
+      const error: errors = {
+        body: [
+          {
+            name: "ValidationError",
+            isJoi: true,
+            annotate(stripColors) {
+              return "";
+            },
+            _original: "",
+            message: "'password' is required",
+            details: [
+              {
+                message: "",
+                path: [""],
+                type: "",
+              },
+            ],
+          },
+        ],
+      };
+
+      const publicMessage = "'password' is required";
+      const newError = new ValidationError(error, {});
+
+      generalError(
+        newError as unknown as CustomError,
+        req as Request,
+        res as Response,
+        next
+      );
+
+      expect(res.json).toHaveBeenCalledWith({ error: publicMessage });
     });
   });
 });
