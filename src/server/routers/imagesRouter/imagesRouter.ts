c3105bdf414a78100d4f12b6e30729c47d106008
Victor Granda <victorgranda1992@gmail.com>
2023-03-15 12:00:32 +0100

    Add routes and auth to imagesRouter

--- src/server/routers/imagesRouter/imagesRouter.ts
+++ src/server/routers/imagesRouter/imagesRouter.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
+import {
+  getAllImages,
+  getUserImages,
+} from "../../controllers/imagesControllers/imagesControllers.js";
+import auth from "../../middlewares/auth/auth.js";
 
 const getImagesRoute = "/";
 const getUserCollectionRoute = "/my-collection";
 
 const imagesRouter = Router();
 
-imagesRouter.get(getImagesRoute);
-imagesRouter.get(getUserCollectionRoute);
+imagesRouter.get(getImagesRoute, getAllImages);
+imagesRouter.get(getUserCollectionRoute, auth, getUserImages);
 
 export default imagesRouter;
