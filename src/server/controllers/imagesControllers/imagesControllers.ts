a01f52a3b0aec5c4b3afe10a8dac1461bfeca1b3
Victor Granda <victorgranda1992@gmail.com>
2023-03-15 10:52:48 +0100

    Add imagesControllers and its test

---
+++ src/server/controllers/imagesControllers/imagesControllers.ts
@@ -0,0 +1,44 @@
+import { type NextFunction, type Request, type Response } from "express";
+import CustomError from "../../../CustomError/CustomError.js";
+import { type CustomRequest } from "../../../types.js";
+import { Image } from "../../../database/models/ImagesModel/Images.js";
+import errors from "../../../constants/errors";
+export const getAllImages = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const images = await Image.find().exec();
+
+    res.status(200).json({ images });
+  } catch (error) {
+    const customError = new CustomError(
+      errors.badRequest.message,
+      errors.badRequest.statusCode,
+      "Couldn't retrieve images"
+    );
+
+    next(customError);
+  }
+};
+
+export const getUserImages = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const images = await Image.find({ promptedBy: req.promptedBy }).exec();
+
+    res.status(200).json({ images });
+  } catch (error) {
+    const customError = new CustomError(
+      errors.badRequest.message,
+      errors.badRequest.statusCode,
+      "Couldn't retrieve images"
+    );
+
+    next(customError);
+  }
+};
